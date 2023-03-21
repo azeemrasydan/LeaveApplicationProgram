95e25ca6f03b9dca1e7e0a7f4dc7b98a6c5787b1
Azeem Rasydan <azeem.rasydan@plus.com.my>
2023-03-21 23:35:26 +0800

    :construction: On-going development on LeaveApplication form submit

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,16 +1,31 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 
 namespace LeaveApplicationProgram.Controllers
 {
+
+    public class User
+    {
+        public string Email;
+    }
+
     public class HomeController : Controller
     {
         public ActionResult Index()
         {
             return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include ="Email")] User user)
+        {
+            var userEmail = user.Email;
+            return RedirectToAction("Index", "Home");
         }
 
         public ActionResult About()
